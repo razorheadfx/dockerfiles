54f76ebeb28ed62f3874a7947053df0cbc4910a4
felix.girke <felix.girke@tu-dortmund.de>
2019-09-16 15:19:22 +0200

    Figure out privileges for arv-viewer

--- aravis/arv-viewer.sh
+++ aravis/arv-viewer.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # before starting this script run: xhost +local:docker
 # to allow docker access to the x socket
+
+# aravis needs access to the interfaces for the discovery part so we need net=host
+
 docker run --rm -i -t \
+    --net=host \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --cap-add NET_ADMIN \
     aravis \
-        /bin/bash -c "LD_LIBRARY_PATH=/usr/local/lib/ NO_AT_BRIDGE=1 arv-viewer"
+          /bin/bash -c "LD_LIBRARY_PATH=/usr/local/lib/ NO_AT_BRIDGE=1 arv-viewer"
